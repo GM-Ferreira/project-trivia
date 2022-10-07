c9ebc32318b243609fa108841c693c9b89a7e75f
João Henrique <joaohenriqueblocked@gmail.com>
2022-10-07 17:33:38 -0300

    Começando requisito 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Switch, Route } from 'react-router-dom';
-import Login from './page/Login';
+import Feedback from './pages/Feedback';
+import Game from './pages/Game';
+import Login from './pages/Login';
+import Ranking from './pages/Ranking';
 
 class App extends React.Component {
   render() {
     return (
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route exact path="/game" component={ Game } />
+        <Route exact path="/feedback" component={ Feedback } />
+        <Route exact path="/ranking" component={ Ranking } />
       </Switch>
     );
   }
