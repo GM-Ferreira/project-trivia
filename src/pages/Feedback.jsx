5c7c9189f39f3daced4353425a428f2a1e9b8639
Robert Amorim <robertamorimbot@gmail.com>
2022-10-11 20:00:57 -0300

    req 15 complit

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -2,16 +2,30 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
+import './Feedback.css';
+
 class Feedback extends Component {
+  handleClick = () => {
+    const { history } = this.props;
+    history.push('/');
+  };
+
   render() {
     const { assertions } = this.props;
     const correctAnswers = 3;
     return (
-      <div data-testid="feedback-text">
-        Feedbacks
+      <div className="feedback" data-testid="feedback-text">
+        <h1>Feedbacks</h1>
         <p data-testid="feedback-text">
           { assertions >= correctAnswers ? 'Well Done!' : 'Could be better...' }
         </p>
+        <button
+          type="button"
+          data-testid="btn-play-again"
+          onClick={ this.handleClick }
+        >
+          Play Again
+        </button>
       </div>
     );
   }
